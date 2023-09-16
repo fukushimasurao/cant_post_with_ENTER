e112a892b608ba3c170ac51791b75172444fc50b
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2023-09-16 20:21:59 +0900

    jsの中身追加

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,9 @@
-console.log("testaaaaaaa");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("keydown", (event) => {
+    // キーコードが13の場合はEnterキー
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  });
+});
